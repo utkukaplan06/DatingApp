527f41cfdb5fc21557dfc4807238daa86bc6280c
unknown <utkukaplan06@hotmail.com>
2020-05-02 10:46:46 +0300

    SeedData

--- DatingApp.API/Data/Datacontext.cs
+++ DatingApp.API/Data/Datacontext.cs
@@ -7,10 +7,12 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) :base  (options) {}
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-         public DbSet<Value>  Values { get; set; }
+        public DbSet<Value> Values { get; set; }
 
-         public DbSet<User> Users { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
     }
-} 
+}
