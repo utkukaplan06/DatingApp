6f7e519d44b5cd7daa4bc6d3118641eecd49cc70
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2025-12-17 16:39:16 +0300

    First commit

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //localhost:5001/api/members
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+
+            return members;
+        }
+
+        [HttpGet("{id}")] //localhost:5001/api/members/bob-id
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await context.Users.FindAsync(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return member;
+        }
+    }
+}
