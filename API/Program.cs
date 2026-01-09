ee54a4bf4881daf4d4f958fc225dbfeff878a019
utkukaplan06 <63205557+utkukaplan06@users.noreply.github.com>
2026-01-09 16:56:45 +0300

    Bölüm 3.25

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod()
+            .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit{
+    
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers(){
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 }
